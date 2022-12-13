c0b9aca63a91b186104f5220eca480c05e0f8e9d
mralvisp <120132243+mralvisp@users.noreply.github.com>
2022-12-13 03:21:26 -0500

    Improvements and Added Section

--- javaScript.js
+++ javaScript.js
@@ -1,18 +1,70 @@
 const btnScrollToTop = document.querySelector('#btnScrollToTop');
-btnScrollToTop.addEventListener('click', function(){
-    
-    
-    window.scrollTo({
-        top:0,
-        left:0,
-        behavior: "smooth"
-      });
-    });
-    
-    
-    
-    $("img").toggle(100);
-    $("img").toggle(3000);
-    $("p").slideUp(100);
-    $("p,h1").slideDown(1500);
-    $("body").fadeIn(3000);
+btnScrollToTop.addEventListener('click', function () {
+
+
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
+});
+
+
+
+$("img").toggle(100);
+$("img").toggle(3000);
+$("p").slideUp(100);
+$("p,h1").slideDown(1500);
+$("body").fadeIn(3000);
+
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+
+$(document).ready(function () {
+  $('.youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 300,
+    preloader: false,
+
+    fixedContentPos: false
+  });
+});
+
+
+
+// Mause Wheel Scroll 
+
+
+function wheel(event) {
+  var delta = 0;
+  if (event.wheelDelta) { (delta = event.wheelDelta / 120); }
+  else if (event.detail) { (delta = -event.detail / 3); }
+
+  handle(delta);
+  if (event.preventDefault) { (event.preventDefault()); }
+  event.returnValue = false;
+}
+
+function handle(delta) {
+  var time = 500;
+  var distance = 300;
+
+  $('html, body').stop().animate({
+    scrollTop: $(window).scrollTop() - (distance * delta)
+  }, time);
+}
+
+if (window.addEventListener) { window.addEventListener('DOMMouseScroll', wheel, false); }
+window.onmousewheel = document.onmousewheel = wheel;
+
+
+
+
